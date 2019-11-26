60b5f177f19640307214bf67f1e1839e25e946de
Alexandre <mandra80080@gmail.com>
2019-11-26 12:40:53 +0100

    Envoie_Exercice1

---
+++ exercise-1/string.js
@@ -0,0 +1,57 @@
+function ucfirst(str) {
+    if(typeof str !== "string" || str.length === 0) return "";
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(ucfirst('ucfirst'));
+console.log(ucfirst('Ucfirst'));
+console.log(ucfirst('5cfirst'));
+console.log(ucfirst('ucfirst test'));
+console.log(ucfirst(''));
+console.log(ucfirst(null));
+console.log(ucfirst({}));
+
+function capitalize(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for (let i = 0; i < array.length;) {
+        array[i] = ucfirst(array[i++]);
+    }
+    return array.join(" ");
+}
+
+console.log(capitalize('capitalize'));
+console.log(capitalize('Capitalize'));
+console.log(capitalize('5apitalize'));
+console.log(capitalize('capitalize test'));
+console.log(capitalize(''));
+console.log(capitalize(null));
+console.log(capitalize({}));
+
+function camelCase(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+
+    const array = str.toLowerCase().split(" ");
+    for (let i = 0; i < array.length;) {
+        array[i] = ucfirst(array[i++]);
+    }
+    return array.join("");
+}
+console.log(camelCase('capitalize test'));
+console.log(camelCase(''));
+console.log(camelCase(null));
+console.log(camelCase({}));
+
+function snake_case(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+
+    const array = str.toLowerCase().split(" ");
+    for (let i = 0; i < array.length;) {
+        array[i] = ucfirst(array[i++]);
+    }
+    return array.join("_");
+}
+console.log(snake_case('capitalize test'));
+console.log(snake_case(''));
+console.log(snake_case(null));
+console.log(snake_case({}));
