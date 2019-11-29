c2a91fb081993f3d6d4b6101fcedd6dfd8e55634
Alexandre <mandra80080@gmail.com>
2019-11-29 14:49:51 +0100

    test-typecheckV1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -6,10 +6,10 @@ function type_check_v1(arg1,arg2) {
     return typeofarg1===typeofarg2;
 }
 console.log(type_check_v1(arg1=42,arg2='hello'));
-//function type_check_v2() {
+function type_check_v2() {
 
-//}
+}
 
-//function type_check_v3() {
+function type_check_v3() {
 
-//}
+}
