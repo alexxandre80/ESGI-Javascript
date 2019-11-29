6e5fa5aab5c55f1fcf3d6e2d8aa83fde47d83da8
Alexandre <mandra80080@gmail.com>
2019-11-29 14:54:48 +0100

    test-typecheckV1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,6 +1,4 @@
 function type_check_v1(arg1,arg2) {
-    if (typeof arg1.length === 0) return "";
-    if (typeof arg2.length === 0) return "";
     const typeofarg1 = typeof arg1;
     const typeofarg2 = typeof arg2;
     return typeofarg1===typeofarg2;
