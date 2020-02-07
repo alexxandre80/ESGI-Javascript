5115292c596f3a6430475d2c97bc741852256826
Alex <amandrake80@gmail.com>
2020-02-07 15:44:20 +0100

    type_check_v2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -8,10 +8,32 @@ function type_check_v1(vari,type) {
    }
 }
 
-function type_check_v2() {
-
+function type_check_v2(vari,type) {
+	for (key in type) {
+		switch (key) {
+			case "type":
+				if (!type_check_v1(vari, type.type)) return false;
+				break;
+			case "value":
+				if(JSON.stringify(vari) !== JSON.stringify(type.value)) {
+                    return false;
+                };
+                break;
+			case "enum":
+				type.enum.forEach(val => {
+					if (!type_check_v2(vari, { value: val })) {
+						return false;
+					}
+					return true;
+				});
+        }
+    }
+    return true;
 }
 
 function type_check_v3() {
 
 }
+
+
+
